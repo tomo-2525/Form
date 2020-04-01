c409c035f5a874f265a9c8e8e172286265495ad9
tomo-2525 <tomoblog.0205@gmail.com>
2020-04-02 01:58:47 +0900

    update

--- js/main.js
+++ js/main.js
@@ -1,19 +1,16 @@
 $(function () {
 
-
-    $("form").submit(fuction(){
+    $("form").submit(function () {
         var checkSubmit = true;
 
-        if($("#uname").val() == ""){
-        checkSubmit = false;
-    }
+        if ($("#uname").val() == "") {
+            checkSubmit = false;
+        }
 
-    if ($(":radio:checked").length == 0) {
-        checkSubmit = false;
-    }
+        if ($(":radio:checked").length == 0) {
+            checkSubmit = false;
+        }
 
-    return checkSubmit;
-});
-    
-
+        return checkSubmit;
+    });
 });
