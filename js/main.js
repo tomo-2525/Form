d7095f96c84a05308294ef5a36fef5c694cc381b
tomo-2525 <tomoblog.0205@gmail.com>
2020-04-02 01:19:13 +0900

    update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-
 $(function () {
 
-    $("form").submit(fuction(){
 
+    $("form").submit(fuction(){
         var checkSubmit = true;
 
-    });
+        if($("#uname").val() == ""){
+        checkSubmit = false;
+    }
+
+    if ($(":radio:checked").length == 0) {
+        checkSubmit = false;
+    }
+
+    return checkSubmit;
+});
+    
+
 });
