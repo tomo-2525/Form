b7c8d68a535c69e31591a0028cd997345ab646c2
tomo-2525 <tomoblog.0205@gmail.com>
2020-04-02 15:14:08 +0900

    update

--- js/main.js
+++ js/main.js
@@ -4,13 +4,55 @@ $(function () {
         var checkSubmit = true;
 
         if ($("#uname").val() == "") {
+            $("#uname").addClass("error");
+            $("#unae+span").text("名前を入力してください。");
             checkSubmit = false;
+        } else {
+            $("#uname").removeClass("error");
+            $("#unae+span").text("");
         }
 
         if ($(":radio:checked").length == 0) {
+            $(":radio").addClass("error");
+            $(":radio+span").text("性別を入力してください。");
             checkSubmit = false;
+        } else {
+            $(":radio").removeClass("error");
+            $(":radio+span").text("");
         }
 
         return checkSubmit;
     });
+
+    $("#umail").change(function () {
+        if ($("#umail").val().match(/^[\w\.\-]+\@[\w\.\-]+$/)) {
+            $("#umail").removeClass("error");
+            $("#umail+span").text("");
+        } else {
+            $("#umail").addClass("error");
+            $("#umail+span").text("書式に誤りがあります。");
+        }
+    });
+
+    $("#uzip").change(function () {
+        if ($("#uzip").val().match(/^\d{3}\-\d{4}$/)) {
+            $("#uzip").removeClass("error");
+            $("#uzip+span").text("");
+        } else {
+            $("#uzip").addClass("error");
+            $("#uzip+span").text("書式に誤りがあります。");
+        }
+    });
+
+    $("textarea").keyup(function () {
+        var inputLength = $("textarea").val().length;
+        if (inputLength <= 100) {
+            $("textarea").removeClass("error");
+            $("textarea+span").text(inputLength + "/100");
+        } else {
+            $("textarea").addClass("error");
+            $("textarea+span").text("入力文字数が長すぎます。");
+        }
+    });
+
 });
